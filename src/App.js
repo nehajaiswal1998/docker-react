93be362f3ed94c05433ffa645e682282fb471bd5
sadaf shaikh <smshaikh9178@gmail.com>
2022-04-11 23:18:23 +0530

    react deploy ebs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          welcome to nehas app
+          welcome to nehas app huh
         </p>
         <a
           className="App-link"
